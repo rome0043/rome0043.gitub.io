723da43682c39a39079c216ae289e175bc284aac
rome0043 <80677958+rome0043@users.noreply.github.com>
2024-05-05 18:13:32 +0930

    Update loginfunctions.js

--- loginfunctions.js
+++ loginfunctions.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
     if ( username == "clouddev" && password == "passintocloud"){
         alert ("Login success");
-        window.location = "welcome.html"; // Redirecting to other page.
+        window.location = "/welcome.html"; // Redirecting to other page.
         return false;
         }
     else{
